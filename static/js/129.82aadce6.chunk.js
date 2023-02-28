5e26bb9a6f240d253a3c61f6b1545cc1c3f30f99
WebHawky <102989300+WebHawky@users.noreply.github.com>
2023-02-28 11:57:38 +0000

    Deploying to gh-pages from @ WebHawky/goit-react-hw-5-movies@a2331239aac892ee9ae89eea215dcf3a83384fcb 🚀

---
+++ static/js/129.82aadce6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_5_movies=self.webpackChunkgoit_react_hw_5_movies||[]).push([[129],{2129:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var u=t(184);function r(){return(0,u.jsx)("h1",{children:"404 Page not found"})}}}]);
+//# sourceMappingURL=129.82aadce6.chunk.js.map
